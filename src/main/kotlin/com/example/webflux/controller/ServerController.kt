f7170c011863a80d08b4d216b1c9b5c6bcf4d764
KimTaeO <taeo0328@gmail.com>
2023-09-29 23:14:11 +0900

    flux를 고차함수로 변환해서 반환

--- src/main/kotlin/com/example/webflux/controller/ServerController.kt
+++ src/main/kotlin/com/example/webflux/controller/ServerController.kt
@@ -3,6 +3,7 @@ package com.example.webflux.controller
 import com.example.webflux.entity.Dish
 import com.example.webflux.service.KitchenService
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -14,5 +15,11 @@ class ServerController(
     @GetMapping(value = ["/server"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun serveDishes(): Flux<Dish> {
         return kitchenService.getDishes()
+    }
+
+    @GetMapping(value = ["/served-dishes"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun deliverDishes(): Flux<Dish> {
+        return kitchenService.getDishes()
+            .map { Dish.deliver(it) }
     }
 }
