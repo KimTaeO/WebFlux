2afebc0f088a1d3826d488aa696e5c942bd19a38
KimTaeO <taeo0328@gmail.com>
2023-09-30 19:16:27 +0900

    도메인 모델 정의

---
+++ src/main/java/com/example/webflux/entity/Item.java
@@ -0,0 +1,55 @@
+package com.example.webflux.entity;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+class Item {
+    private @Id String id;
+    private String name;
+    private double price;
+
+    public Item() {}
+
+    Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Double.compare(price, item.price) == 0 && Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
