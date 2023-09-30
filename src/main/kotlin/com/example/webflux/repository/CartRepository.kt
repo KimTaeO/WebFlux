60b223aff5aa6aaa9cd18b0ed420440a2120d829
KimTaeO <taeo0328@gmail.com>
2023-09-30 18:50:04 +0900

    리셋

--- src/main/kotlin/com/example/webflux/repository/CartRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.webflux.repository
-
-import com.example.webflux.entity.Cart
-import org.springframework.data.mongodb.repository.Query
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface CartRepository : ReactiveCrudRepository<Cart, String> {
-}
--- src/main/kotlin/com/example/webflux/repository/ItemRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.webflux.repository
-
-import com.example.webflux.entity.Item
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface ItemRepository : ReactiveCrudRepository<Item, String> {
-}
