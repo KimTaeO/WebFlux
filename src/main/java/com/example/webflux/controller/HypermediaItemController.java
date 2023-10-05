769c7dfcc257a253fca7be1615b8b34a97402c72
KimTaeO <taeo0328@gmail.com>
2023-10-05 14:20:05 +0900

    헤이티오스를 사용한 하이퍼미디어 컨트롤러 작성

--- src/main/java/com/example/webflux/controller/HypermediaItemController.java
+++ src/main/java/com/example/webflux/controller/HypermediaItemController.java
@@ -10,6 +10,7 @@ import org.springframework.hateoas.Links;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
@@ -20,16 +21,22 @@ import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.met
 public class HypermediaItemController {
     private final ItemRepository repository;
 
+
     @GetMapping("/hypermedia/items/{id}")
     Mono<EntityModel<Item>> findOne(@PathVariable String id) {
         HypermediaItemController controller = methodOn(HypermediaItemController.class);
 
         Mono<Link> selfLink = linkTo(controller.findOne(id)).withSelfRel().toMono();
 
-        Mono<Link> aggregateLink = linkTo(repository.findAll())
+        Mono<Link> aggregateLink = linkTo(controller.findAll())
                 .withRel(IanaLinkRelations.ITEM).toMono();
 
         return Mono.zip(repository.findById(id), selfLink, aggregateLink)
                 .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
+    }
+
+    @GetMapping("/hypermedia/api/items")
+    Flux<Item> findAll() {
+        return repository.findAll();
     }
 }
